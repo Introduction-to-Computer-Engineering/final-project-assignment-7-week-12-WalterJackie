c358a76e9759e2a6cdf6cdda206e86a7535c1802
WalterJackie <54460872+WalterJackie@users.noreply.github.com>
2019-11-11 10:35:39 -0700

    Update (coments needed)manual-calibration.js

--- (coments needed)manual-calibration.js
+++ (coments needed)manual-calibration.js
@@ -1,31 +1,30 @@
-let value1 = 0
+let value1 = 0 //value1 is the value of pin 0
 
-basic.forever(function () {
-    let run = new read()
-    run.do_read()
-    basic.pause(1000)
-    basic.clearScreen()
-    basic.pause(100)
+basic.forever(function () { //forever 
+    let run = new read() //creats new instance of read class
+    run.do_read() //begins read cycle
+    basic.pause(1000) //pause
+    basic.clearScreen() //reeset for next reading
+    basic.pause(100) //pause
 })
 
 
-class read {
-    constructor() { }
-    do_read() {
-        pins.digitalWritePin(DigitalPin.P12, 1);
-        value1 = pins.analogReadPin(AnalogPin.P0);
-        pins.digitalWritePin(DigitalPin.P12, 0);
-        let i: number = pins.map(value1, 8, 815, 0, 4);
-        this.fill_chart(i)
-        this.make_chart(i)
+class read {    //where the magic happens
+    do_read() { //read and display
+        pins.digitalWritePin(DigitalPin.P12, 1);//start mesuring
+        value1 = pins.analogReadPin(AnalogPin.P0);//set value1 to analog values of pin 0
+        pins.digitalWritePin(DigitalPin.P12, 0);//stops mesuring
+        let i: number = pins.map(value1, 8, 815, 0, 4);//creats var i = a mesurment from 0 to 4
+        this.fill_chart(i)//fills the chart with starting points
+        this.make_chart(i)//fills the chart with mesurment
     }
     fill_chart(r: number) {
         for (let i = r; i >= 0; i--) {
-            this.make_chart(i)
+            this.make_chart(i)//runs till chart is full
         }
 
     }
-    make_chart(r: number): void {
+    make_chart(r: number): void {//fills chart
         led.plot(0, 4 - r);
         led.plot(1, 4 - r);
         led.plot(2, 4 - r);
